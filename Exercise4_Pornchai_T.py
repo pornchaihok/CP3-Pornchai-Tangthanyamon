1878359badd32d12ca78f89255582fab27da0bfa
pornchaihok <hok.pornchai@gmail.com>
2020-11-01 19:55:18 +0700

    Create Exercise4_Pornchai_T.py

---
+++ Exercise4_Pornchai_T.py
@@ -0,0 +1,13 @@
+fe = 75
+gb = 73
+ics = 82
+cp = 69
+print("--- Your Score ---")
+
+print("Foundation English :", fe)
+
+print("General Business :", gb)
+
+print("Introduction to Computer Systems :", ics)
+
+print("Computer Programming :", cp)
